39bf45cad53391d46f9c90107cbe6a86e3166971
mnapoleon <michael.napoleon@gmail.com>
2022-12-17 11:06:21 -0500

    increase code coverage
    more code coverage additions

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,19 +1,82 @@
 """Test cases for the console module."""
+from unittest.mock import patch
+
 import click.testing
 import pytest
 
-
 from render_cli import console
+from . import test_constants
 
 
-@pytest.fixture
-def runner():
-    """Fixture for invoking command-line interfces."""
-    return click.testing.CliRunner()
+class TestConsole:
+    """Test console."""
 
+    @pytest.fixture
+    def runner(self):
+        """Fixture for invoking command-line interfces."""
+        return click.testing.CliRunner()
 
-@pytest.mark.e2e
-def test_main_succeeds(runner):
-    """It exits with a status code of zero."""
-    result = runner.invoke(console.cli)
-    assert result.exit_code == 0
+    @pytest.fixture(scope="class")
+    def fetch_services_response(self):
+        """Fixture for fetching services."""
+        return test_constants.fetch_services_response
+
+    @pytest.mark.e2e
+    def test_main_succeeds(self, runner):
+        """It exits with a status code of zero."""
+        result = runner.invoke(console.cli)
+        assert result.exit_code == 0
+
+    def test_list_services_command(self, runner, fetch_services_response):
+        """Tests console call to fetch-services command."""
+        with patch("render_cli.console.rs") as mock_render_services:
+            mock_render_services.fetch_services.return_value = (
+                test_constants.test_services
+            )
+            # test verbose mode
+            result = runner.invoke(console.cli, ["list", "-v"])
+            assert result.exit_code == 0
+
+            # test normal mode
+            result = runner.invoke(console.cli, ["list"])
+            assert result.exit_code == 0
+
+    def test_find_service_command(self, runner):
+        """Test console call to find_service_by_name."""
+        with patch("render_cli.console.rs") as mock_render_services:
+            mock_render_services.find_service_by_name.return_value = (
+                test_constants.test_svc_1
+            )
+            # test verbose mode
+            result = runner.invoke(
+                console.cli, ["find-service", "-sn", "test-service-name", "-v"]
+            )
+            assert result.exit_code == 0
+
+            # test normal mode
+            result = runner.invoke(
+                console.cli, ["find-service", "-sn", "test-service-name"]
+            )
+            assert result.exit_code == 0
+
+    def test_list_env_command(self, runner):
+        """Test console call fetch env vars for a service."""
+        with patch("render_cli.console.rs") as mock_render_services:
+            mock_render_services.find_service_by_name.return_value = (
+                test_constants.test_svc_1
+            )
+            mock_render_services.retrieve_env_from_render.return_value = (
+                test_constants.test_env_vars
+            )
+
+            # test verbose mode
+            result = runner.invoke(
+                console.cli, ["list-env", "-sn", "test-service-name", "-v"]
+            )
+            assert result.exit_code == 0
+
+            # test normal mode
+            result = runner.invoke(
+                console.cli, ["list-env", "-sn", "test-service-name"]
+            )
+            assert result.exit_code == 0
